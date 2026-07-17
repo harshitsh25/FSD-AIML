dae168f33a0f548193f5e56bfc3ac233580ff463
Harshit <wragnerharshit@gmail.com>
2026-07-17 14:44:48 +0530

    created ex-1.js

---
+++ ex-2.js
@@ -0,0 +1,9 @@
+const a = [1,2,3,4,5,6,7,8,9,20];
+const b = a.filter((i) => (i % 2 == 0));
+console.log("Even = ", b);
+const c = b.map((i) => (i*i));
+console.log("Square = ", c);
+const d = c.reduce((i, s) => s + i);
+console.log("Sum = ",d);
+
+
